7f46cc473b6700f43d18cd10e791cdc612ebfbe9
Gamil Abdullah <89533714+gamilabd@users.noreply.github.com>
2022-02-07 06:26:22 +0300

    created UPDATE USERs in admins

--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from users.models import User
-from admins.forms import UserAdminRegistrationForm
+from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 from django.urls import reverse
 
 # Create your views here.
@@ -30,10 +30,22 @@ def admin_users(request):
     return render(request, 'admins/admin-users-read.html', context)
 
 #ubdate
-def admin_users_update(request):
-    context = {'title': 'Geekshop - Обнавление пользователя'}
+def admin_users_update(request, id):
+    selected_user = User.objects.get(id=id)
+    if request.method == "POST":
+        form = UserAdminProfileForm(instance=selected_user, files=request.FILES, data=request.POST)
+        if form.is_valid:
+            form.save()
+        return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance=selected_user)
+    context = {
+        'title': 'Geekshop - Обнавление пользователя',
+        'form': form,
+        'selected_user': selected_user,
+               }
     return render(request, 'admins/admin-users-update-delete.html', context)
 
 #delete
-def admin_users_delete(request):
+def admin_users_remove(request):
     pass
