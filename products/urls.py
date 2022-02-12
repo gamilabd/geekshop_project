36d0d96443634511cf890ca42ef5eca44397fe46
Gamil Abdullah <89533714+gamilabd@users.noreply.github.com>
2022-02-13 00:18:21 +0300

    activation productCategory for products

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,8 @@ from products.views import products
 app_name = 'products'
 
 urlpatterns = [
-    path('', products, name='index'),
-    path('<int:pk>/', products, name='category'),
+    path('', products, name='index'), #.../products/
+    #path('<int:pk>/', products, name='categori'),
+    path('<int:category_id>/', products, name='category'), #.../products/category_id/
+    path('page/<int:page>/', products, name='page'), #.../products/category_id/
 ]
--- products/views.py
+++ products/views.py
@@ -6,6 +6,7 @@ import os
 import json
 from datetime import datetime
 from products.models import ProductCategory, Product
+from django.core.paginator import Paginator, EmptyPage, Page
 
 MODULE_DIR = os.path.dirname(__file__)  # в этой переменной будем сдержать путь до папки products
 
@@ -18,12 +19,14 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request, pk=None):
-    print(pk)
-    file_path = os.path.join(MODULE_DIR, 'fixtures/goods.json')  # содержить польный путь до файла goods.json
-    context = {
-        'title': 'GeekShop-Каталог',
-        'categories': ProductCategory.objects.all(),
-        'products': Product.objects.all(),
-    }
+def products(request, category_id=None, page=1, pk=None):  #(request, category_id=None, pk=None)
+    #print(pk)
+    #file_path = os.path.join(MODULE_DIR, 'fixtures/goods.json')  # содержить польный путь до файла goods.json
+    context = {'title': 'GeekShop-Каталог', 'categories': ProductCategory.objects.all()}
+    if category_id:
+        category = ProductCategory.objects.get(id=category_id)
+        products = Product.objects.filter(category=category)
+    else:
+        products = Product.objects.all()
+    context['products'] = products
     return render(request, 'products/products.html', context)
