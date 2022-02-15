76c50d2414387c9f556780afa5ee20be49fdc90b
Gamil Abdullah <89533714+gamilabd@users.noreply.github.com>
2022-02-15 16:06:48 +0300

    added UpdateView

--- admins/urls.py
+++ admins/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from admins.views import index, UserCreateView, admin_users_update, admin_users_remove, UserListView
+from admins.views import index, UserCreateView, UserUpdateView, admin_users_remove, UserListView
 
 app_name = 'admins'
 
@@ -9,6 +9,6 @@ urlpatterns = [
     #path('users/', admin_users, name='admin_users'),
     path('users/', UserListView.as_view(), name='admin_users'),
     path('users/create/', UserCreateView.as_view(), name='admin_users_create'),
-    path('users/update/<int:id>/', admin_users_update, name='admin_users_update'),
+    path('users/update/<int:pk>/', UserUpdateView.as_view(), name='admin_users_update'),
     path('users/remove/<int:id>/', admin_users_remove, name='admin_users_remove'),
 ]
